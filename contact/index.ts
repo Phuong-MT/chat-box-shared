b3db07ce71526bc65dbe2bd014ff05d6175efeb7
minh phuong <phuong.tm225381@sis.hust.edu.vn>
2025-09-23 18:16:29 +0700

    add mimeType file

--- contact/index.ts
+++ contact/index.ts
@@ -9,4 +9,25 @@ export class Contacts {
     MANAGEMENT: 'MANAGEMENT',
     MEMBER: 'MEMBER',
   };
+  static MimeTypeFile = {
+    // Ảnh
+    IMAGE_PNG: 'image/png',
+    IMAGE_JPEG: 'image/jpeg',
+    IMAGE_GIF: 'image/gif',
+
+    // PDF
+    PDF: 'application/pdf',
+
+    // Word
+    DOC: 'application/msword',
+    DOCX: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+
+    // Excel
+    XLS: 'application/vnd.ms-excel',
+    XLSX: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+
+    // PowerPoint
+    PPT: 'application/vnd.ms-powerpoint',
+    PPTX: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+  };
 }
