8ab1a27d77d1c46966880ee495bcd9707a1135c0
minh phuong <phuong.tm225381@sis.hust.edu.vn>
2025-10-16 15:19:39 +0700

    add socket name space

--- contact/index.ts
+++ contact/index.ts
@@ -1,43 +1,47 @@
+export enum NAME_SPACE_SOCKET {
+    CHAT_BOX = "chat-box",
+}
+
 export class Contacts {
-  static GroupType = {
-    DIRECT: 'DIRECT',
-    GROUP: 'GROUP',
-  };
-  //
-  static RoleGroupMembers = {
-    ADMIN: 'ADMIN',
-    MANAGEMENT: 'MANAGEMENT',
-    MEMBER: 'MEMBER',
-  };
-  static MimeTypeFile = {
-    // Ảnh
-    IMAGE_PNG: 'image/png',
-    IMAGE_JPEG: 'image/jpeg',
-    IMAGE_GIF: 'image/gif',
+    static GroupType = {
+        DIRECT: "DIRECT",
+        GROUP: "GROUP",
+    };
+    //
+    static RoleGroupMembers = {
+        ADMIN: "ADMIN",
+        MANAGEMENT: "MANAGEMENT",
+        MEMBER: "MEMBER",
+    };
+    static MimeTypeFile = {
+        // Ảnh
+        IMAGE_PNG: "image/png",
+        IMAGE_JPEG: "image/jpeg",
+        IMAGE_GIF: "image/gif",
 
-    // PDF
-    PDF: 'application/pdf',
+        // PDF
+        PDF: "application/pdf",
 
-    // Word
-    DOC: 'application/msword',
-    DOCX: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        // Word
+        DOC: "application/msword",
+        DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
 
-    // Excel
-    XLS: 'application/vnd.ms-excel',
-    XLSX: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        // Excel
+        XLS: "application/vnd.ms-excel",
+        XLSX: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 
-    // PowerPoint
-    PPT: 'application/vnd.ms-powerpoint',
-    PPTX: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-  };
-  static jwt = {
-    ACCESS_TOKEN: 'access_token',
-    REFRESH_TOKEN: 'refresh_token',
-  };
-  static CHAT_BOX_PATH = '/chat-box';
-  static SUBSCRIBE_MESSAGE = {
-    CHAT_BOX: {
-      SEND_MESSAGE: `${this.CHAT_BOX_PATH}/send-message`,
-    },
-  };
+        // PowerPoint
+        PPT: "application/vnd.ms-powerpoint",
+        PPTX: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    };
+    static jwt = {
+        ACCESS_TOKEN: "access_token",
+        REFRESH_TOKEN: "refresh_token",
+    };
+
+    static SUBSCRIBE_MESSAGE = {
+        CHAT_BOX: {
+            SEND_MESSAGE: `${NAME_SPACE_SOCKET.CHAT_BOX}/send-message`,
+        },
+    };
 }
